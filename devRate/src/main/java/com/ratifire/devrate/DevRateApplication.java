6d0222a18daad2fbef363561c5981c71c499901d
dante <shaitanaleksander@gmail.com>
2024-01-07 21:08:01 +0200

    initialized spring boot app with spring MVC and spring test

---
+++ devRate/src/main/java/com/ratifire/devrate/DevRateApplication.java
@@ -0,0 +1,13 @@
+package com.ratifire.devrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevRateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevRateApplication.class, args);
+    }
+
+}
---
+++ devRate/src/test/java/com/ratifire/devrate/DevRateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratifire.devrate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevRateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
