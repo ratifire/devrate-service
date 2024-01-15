53f82b06dcbc326e2fda3a67afd8391119c6c4e4
kyva22 <tyrtyrforit@gmail.com>
2024-01-15 19:11:53 +0200

    Path to Api created as property

--- src/main/java/com/ratifire/devrate/controller/HealthCheckController.java
+++ src/main/java/com/ratifire/devrate/controller/HealthCheckController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/healthCheck")
+@RequestMapping("${api.base.path}/healthCheck")
 public class HealthCheckController {
 
     @GetMapping
