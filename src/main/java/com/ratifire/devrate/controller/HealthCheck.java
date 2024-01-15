3533d93be421579327ed8bcb372934c836138fa6
kyrylovasylchenko <121287345+kyrylovasylchenko@users.noreply.github.com>
2024-01-15 17:37:39 +0200

    Update HealthCheck java class name

--- src/main/java/com/ratifire/devrate/controller/HealthCheck.java
+++ src/main/java/com/ratifire/devrate/controller/HealthCheck.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/healthCheck")
-public class HealthCheck {
+public class HealthCheckController {
 
     @GetMapping
     public ResponseEntity<String> aliveMessage() {
