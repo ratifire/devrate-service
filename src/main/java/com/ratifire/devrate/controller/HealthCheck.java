d26a1558fab429863ec1ffbb29fdf87c944a1831
kyrylovasylchenko <121287345+kyrylovasylchenko@users.noreply.github.com>
2024-01-15 17:26:29 +0200

    Update HealthCheck.java

--- src/main/java/com/ratifire/devrate/controller/HealthCheck.java
+++ src/main/java/com/ratifire/devrate/controller/HealthCheck.java
@@ -1,6 +1,5 @@
 package com.ratifire.devrate.controller;
 
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
