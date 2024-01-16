c7173980f761fbc1b809b657d6851ca2d3b46e9a
hubersky <volodymyr.hubersky@gmail.com>
2024-01-17 01:12:31 +0200

    1. Add test source application.properties 2. Add runtime scope for postgres dependencies

--- src/test/java/com/ratifire/devrate/H2IntegrationTest.java
+++ src/test/java/com/ratifire/devrate/H2IntegrationTest.java
@@ -4,15 +4,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
 
 @SpringBootTest
-@PropertySource({"classpath:application-test.properties"})
 public class H2IntegrationTest {
 
     @Autowired
@@ -23,8 +20,6 @@ public class H2IntegrationTest {
         try (Connection connection = dataSource.getConnection()) {
             Assertions.assertNotNull(connection);
             Assertions.assertFalse(connection.isClosed());
-            DatabaseMetaData metaData = connection.getMetaData();
-            System.out.println("Connected to database: " + metaData.getURL());
         }
     }
 }
