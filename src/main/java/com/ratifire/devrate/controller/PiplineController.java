b0c1e2403a61aaeac716706eda03c185dc915e8c
dante <shaitanaleksander@gmail.com>
2024-02-03 19:54:45 +0200

    smale refactoring

--- src/main/java/com/ratifire/devrate/controller/PiplineController.java
+++ src/main/java/com/ratifire/devrate/controller/PiplineController.java
@@ -4,11 +4,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * The PiplineController class is a REST controller that handles the '/alive' endpoint.
+ * It provides a GET request handler to check if the API is alive and returns a question string.
+ */
 @RestController
 @RequestMapping("/alive")
-public class PiplineController { //todo delete
+public class PiplineController {
 
 
+    /**
+     * Returns a string representing the status of the API.
+     *
+     * @return a string indicating the status of the API
+     */
     @GetMapping
     public String alive(){
         return "what is the time complexity of put method in HashMap?";
