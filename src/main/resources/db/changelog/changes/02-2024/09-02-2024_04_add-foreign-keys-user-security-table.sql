f1cef46074589772d40d6a84354e4879ba57c2f4
Dmytro Abalmas <124811948+abalmasDA@users.noreply.github.com>
2024-02-09 15:04:34 +0200

    minor refactoring

--- src/main/resources/db/changelog/changes/02-2024/09-02-2024_04_add-foreign-keys-user-security-table.sql
+++ src/main/resources/db/changelog/changes/02-2024/09-02-2024_04_add-foreign-keys-user-security-table.sql
@@ -1,3 +1,4 @@
 ALTER TABLE user_security
-    ADD CONSTRAINT fk_user_role FOREIGN KEY (user_role_id) REFERENCES user_role(id),
+    ADD CONSTRAINT fk_user_role FOREIGN KEY (user_role_id) REFERENCES user_role(id);
+ALTER TABLE user_security
     ADD CONSTRAINT fk_user_details FOREIGN KEY (user_id) REFERENCES user_details(id);
