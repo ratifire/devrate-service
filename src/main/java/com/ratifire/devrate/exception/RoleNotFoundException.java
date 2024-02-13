86cf85f05ed4323b3fe1c03a126fa84491e34aa0
O. Voskresenskyy <ovoskresenskyy@gmail.com>
2024-02-13 17:04:05 +0100

    Deleted redundant ResponseStatuses

--- src/main/java/com/ratifire/devrate/exception/RoleNotFoundException.java
+++ src/main/java/com/ratifire/devrate/exception/RoleNotFoundException.java
@@ -1,13 +1,9 @@
 package com.ratifire.devrate.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 /**
  * Exception thrown when a requested role is not found in the system. This exception indicates that
  * the specified role could not be found in the database.
  */
-@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class RoleNotFoundException extends RuntimeException {
 
   public RoleNotFoundException(String message) {
--- src/main/java/com/ratifire/devrate/exception/UserAlreadyExistException.java
+++ src/main/java/com/ratifire/devrate/exception/UserAlreadyExistException.java
@@ -1,14 +1,10 @@
 package com.ratifire.devrate.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 /**
  * Exception thrown when attempting to create a new user with an email address that already exists
  * in the system. This exception indicates that a user with the same email address already exists
  * and prevents duplicate user creation.
  */
-@ResponseStatus(HttpStatus.CONFLICT)
 public class UserAlreadyExistException extends RuntimeException {
 
   /**
