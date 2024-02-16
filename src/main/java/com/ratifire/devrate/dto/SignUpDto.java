5b63b63f3dee513b444cf57f324b18928845fed0
Dmytro Abalmas <124811948+abalmasDA@users.noreply.github.com>
2024-02-16 12:40:09 +0200

    added description to SignUpDto class

--- src/main/java/com/ratifire/devrate/dto/SignUpDto.java
+++ src/main/java/com/ratifire/devrate/dto/SignUpDto.java
@@ -1,5 +1,6 @@
 package com.ratifire.devrate.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,22 +11,31 @@ import lombok.Getter;
  */
 @Builder
 @Getter
+@Schema(description = "Data transfer object for sign-up necessary information")
 public class SignUpDto {
 
+  @Schema(description = "The email chosen by the user for their account")
   private String email;
 
+  @Schema(description = "The first name inputted by user")
   private String firstName;
 
+  @Schema(description = "The last name inputted by user")
   private String lastName;
 
+  @Schema(description = "The country chosen by the user for their account")
   private String country;
 
+  @Schema(description = "Flag indicating whether user is subscribed to newsletter")
   private boolean subscribed;
 
+  @Schema(description = "Flag indicating whether user's email is verified")
   private boolean verified;
 
+  @Schema(description = "The password chosen by the user for their account")
   private String password;
 
+  @Schema(description = "Timestamp when sign-up information was created")
   private LocalDateTime createdAt;
 
 }
