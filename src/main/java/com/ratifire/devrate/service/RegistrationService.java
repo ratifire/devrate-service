a6e95447f091a2c4078ffb74380d0ad3cb7d84a7
NewEnergyCode <matveychuk.m@gmail.com>
2024-02-17 13:15:18 +0200

    upgrade Checkstyle v2

--- src/main/java/com/ratifire/devrate/service/RegistrationService.java
+++ src/main/java/com/ratifire/devrate/service/RegistrationService.java
@@ -17,80 +17,81 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class RegistrationService {
 
-    /**
-     * Service responsible for user management operations.
-     */
-    private final UserService userService;
+  /**
+   * Service responsible for user management operations.
+   */
+  private final UserService userService;
 
-    /**
-     * Service responsible for role management operations.
-     */
-    private final RoleService roleService;
+  /**
+   * Service responsible for role management operations.
+   */
+  private final RoleService roleService;
 
-    /**
-     * Service responsible for user security management operations.
-     */
-    private final UserSecurityService userSecurityService;
+  /**
+   * Service responsible for user security management operations.
+   */
+  private final UserSecurityService userSecurityService;
 
-    private final UserMapper userMapper;
+  private final UserMapper userMapper;
 
-    /**
-     * Service responsible for sending message to the user.
-     */
-    private final GMailSendService gMailSendService;
+  /**
+   * Service responsible for sending message to the user.
+   */
+  private final GMailSendService gmailSendService;
 
-    /**
-     * Checks if a user with the given email address exists.
-     *
-     * @param email The email address to check for existence.
-     * @return True if a user with the specified email address exists, false otherwise.
-     */
-    public boolean isUserExistByEmail(String email) {
-        return userService.isUserExistByEmail(email);
+  /**
+   * Checks if a user with the given email address exists.
+   *
+   * @param email The email address to check for existence.
+   * @return True if a user with the specified email address exists, false otherwise.
+   */
+  public boolean isUserExistByEmail(String email) {
+    return userService.isUserExistByEmail(email);
+  }
+
+  /**
+   * Method for registering a new user. This method validates the sign-up request, creates a new
+   * user entity, and persists it to the database.
+   *
+   * @param signUpDto DTO containing user sign-up data.
+   * @return The registered User object.
+   */
+  @Transactional
+  public User registerUser(SignUpDto signUpDto) {
+    if (isUserExistByEmail(signUpDto.getEmail())) {
+      throw new UserAlreadyExistException("User is already registered!");
     }
 
-    /**
-     * Method for registering a new user. This method validates the sign-up request, creates a new
-     * user entity, and persists it to the database.
-     *
-     * @param signUpDto DTO containing user sign-up data.
-     * @return The registered User object.
-     */
-    @Transactional
-    public User registerUser(SignUpDto signUpDto) {
-        if (isUserExistByEmail(signUpDto.getEmail())) {
-            throw new UserAlreadyExistException("User is already registered!");
-        }
+    User user = userService.save(userMapper.toEntity(signUpDto));
 
-        User user = userService.save(userMapper.toEntity(signUpDto));
+    UserSecurity userSecurity = UserSecurity.builder()
+            .password(signUpDto.getPassword())
+            .userId(user.getId())
+            .userRoleId(roleService.getRoleByName("ROLE_USER").getId())
+            .build();
+    userSecurityService.save(userSecurity);
 
-        UserSecurity userSecurity = UserSecurity.builder()
-                .password(signUpDto.getPassword())
-                .userId(user.getId())
-                .userRoleId(roleService.getRoleByName("ROLE_USER").getId())
-                .build();
-        userSecurityService.save(userSecurity);
+    sendMailAboutSuccessfulRegistration(signUpDto);
 
-        sendMailAboutSuccessfulRegistration(signUpDto);
+    return user;
+  }
 
-        return user;
+  /**
+   * Method for sending the message to the user about successful registration.
+   */
+  public void sendMailAboutSuccessfulRegistration(SignUpDto signUpDto) {
+    String message = String.format("""
+            Dear %s %s,
+                                    
+            Registration is successful!
+                                            
+            Welcome to our platform!
+            """, signUpDto.getFirstName(), signUpDto.getLastName());
+
+    try {
+      gmailSendService.sendMail("Registration is successful", message, signUpDto.getEmail());
+    } catch (Exception e) {
+      throw new RuntimeException(e);
     }
-
-    /**
-     * Method for sending the message to the user about successful registration.
-     */
-    public void sendMailAboutSuccessfulRegistration(SignUpDto signUpDto) {
-        String message = String.format("""
-                Dear %s %s,
-                                        
-                Registration is successful!
-                                                
-                Welcome to our platform!
-                """, signUpDto.getFirstName(), signUpDto.getLastName());
-        try {
-            gMailSendService.sendMail("Registration is successful", message, signUpDto.getEmail());
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+  }
 }
