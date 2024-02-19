52ca0c4f241398c039f4d4798d8f99616bca3429
Ivan Ivashchenko <ivachko2009@gmail.com>
2024-02-19 21:52:23 +0200

    I-52 PR changes

--- src/main/java/com/ratifire/devrate/service/email/EmailService.java
+++ src/main/java/com/ratifire/devrate/service/email/EmailService.java
@@ -17,7 +17,7 @@ import org.springframework.util.StringUtils;
  */
 @AllArgsConstructor
 @Service
-public class EmailService implements EmailSender {
+public class EmailService {
 
   private static final Logger log = LogManager.getLogger(EmailService.class);
 
