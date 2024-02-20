a4266a57e7e9a394ac61af9c37d0a62500c92618
O. Voskresenskyy <ovoskresenskyy@gmail.com>
2024-02-20 17:01:49 +0100

    Add setting createdAt with expression

--- src/main/java/com/ratifire/devrate/mapper/UserMapper.java
+++ src/main/java/com/ratifire/devrate/mapper/UserMapper.java
@@ -3,6 +3,7 @@ package com.ratifire.devrate.mapper;
 import com.ratifire.devrate.dto.SignUpDto;
 import com.ratifire.devrate.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 /**
  * Interface for mapping SignUpDto to User entities.
@@ -10,5 +11,6 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
+  @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")
   User toEntity(SignUpDto signUpDto);
 }
