d0d0fe6340dc1fcc9046ded7c042177c3edce2a2
O. Voskresenskyy <ovoskresenskyy@gmail.com>
2024-02-23 22:15:21 +0100

    Fixed checkstyle issue

--- src/main/java/com/ratifire/devrate/enums/AccessLevel.java
+++ src/main/java/com/ratifire/devrate/enums/AccessLevel.java
@@ -1,9 +1,17 @@
 package com.ratifire.devrate.enums;
 
+/**
+ * Enum representing different access levels in the system.
+ */
 public enum AccessLevel {
   ADMIN("Admin"),
   USER("User");
 
+  /**
+   * Returns the default access level.
+   *
+   * @return The default access level (USER)
+   */
   public static AccessLevel getDefault() {
     return USER;
   }
