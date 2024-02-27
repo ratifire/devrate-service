651d07d2a51daa81f2aedfd95f28b637d55bb75d
O. Voskresenskyy <ovoskresenskyy@gmail.com>
2024-02-27 15:05:16 +0100

    Make User implements UserDetails, to make it simple to work with Spring security entity

--- src/main/java/com/ratifire/devrate/service/UserDetailsServiceImpl.java
+++ src/main/java/com/ratifire/devrate/service/UserDetailsServiceImpl.java
@@ -1,14 +1,10 @@
 package com.ratifire.devrate.service;
 
-import com.ratifire.devrate.entity.User;
-import com.ratifire.devrate.entity.UserSecurity;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Service implementation for retrieving user details.
@@ -18,16 +14,9 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserDetailsServiceImpl implements UserDetailsService {
 
   private final UserService userService;
-  private final UserSecurityService userSecurityService;
 
   @Override
-  @Transactional
   public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-    User user = userService.findUserByEmail(email);
-    UserSecurity userSecurity = userSecurityService.findByUserId(user.getId());
-    return new org.springframework.security.core.userdetails.User(
-        user.getEmail(),
-        userSecurity.getPassword(),
-        List.of(userSecurity.getRole()));
+    return userService.findUserByEmail(email);
   }
 }
