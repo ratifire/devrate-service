4c49cba8fe4f93582e747c0dcdfc47c76abe83e6
O. Voskresenskyy <ovoskresenskyy@gmail.com>
2024-03-10 13:32:28 +0100

    - Add dependency - Create simple class for notification

---
+++ src/main/java/com/ratifire/devrate/entity/Notification.java
@@ -0,0 +1,14 @@
+package com.ratifire.devrate.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * Class representing a notification.
+ */
+@Builder
+@Getter
+public class Notification {
+
+  String text;
+}
