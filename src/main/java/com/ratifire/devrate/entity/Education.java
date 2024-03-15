d87cd6e022b2ff99350ba134f4225b731d761de2
hubersky <volodymyr.hubersky@gmail.com>
2024-03-15 16:16:01 +0200

    Refactor Logic

--- src/main/java/com/ratifire/devrate/entity/Education.java
+++ src/main/java/com/ratifire/devrate/entity/Education.java
@@ -6,7 +6,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
