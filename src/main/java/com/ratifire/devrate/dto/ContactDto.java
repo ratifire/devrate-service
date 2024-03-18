0ca5472e2cf148ffe84fb564e886495af057f4b3
Dmytro Abalmas <124811948+abalmasDA@users.noreply.github.com>
2024-03-18 21:02:19 +0200

    Added class ContactDto

---
+++ src/main/java/com/ratifire/devrate/dto/ContactDto.java
@@ -0,0 +1,13 @@
+package com.ratifire.devrate.dto;
+
+import com.ratifire.devrate.enums.ContactType;
+
+/**
+ * Data Transfer Object (DTO) representing the contact.
+ */
+public class ContactDto {
+
+  private ContactType type;
+  private String value;
+
+}
