ca4330a6a2312cef9e31c14138df5991c7cc5345
Dmytro Abalmas <124811948+abalmasDA@users.noreply.github.com>
2024-03-19 17:08:03 +0200

    Minor refactoring: class ContactDto

--- src/main/java/com/ratifire/devrate/dto/ContactDto.java
+++ src/main/java/com/ratifire/devrate/dto/ContactDto.java
@@ -1,10 +1,14 @@
 package com.ratifire.devrate.dto;
 
 import com.ratifire.devrate.enums.ContactType;
+import lombok.Builder;
+import lombok.Getter;
 
 /**
  * Data Transfer Object (DTO) representing the contact.
  */
+@Builder
+@Getter
 public class ContactDto {
 
   private ContactType type;
