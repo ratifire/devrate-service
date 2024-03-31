7db4bc30d36414846f838bc0b5fd593824e98d76
O. Voskresenskyy <ovoskresenskyy@gmail.com>
2024-04-01 01:20:56 +0200

    WebSocketHandler now can 1. Send notifications when new connection established 2. Handle "read" notification 3. Handle "delete" notification

--- src/main/java/com/ratifire/devrate/util/websocket/WebSocketHandler.java
+++ src/main/java/com/ratifire/devrate/util/websocket/WebSocketHandler.java
@@ -1,6 +1,13 @@
 package com.ratifire.devrate.util.websocket;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ratifire.devrate.dto.NotificationActionDto;
+import com.ratifire.devrate.enums.NotificationAction;
+import com.ratifire.devrate.exception.ActionNotSupportedException;
 import com.ratifire.devrate.exception.LoginNotFoundException;
+import com.ratifire.devrate.exception.WebSocketInvalidMessageFormatException;
+import com.ratifire.devrate.service.NotificationService;
 import java.security.Principal;
 import lombok.AllArgsConstructor;
 import org.springframework.lang.NonNull;
@@ -18,11 +25,16 @@ import org.springframework.web.socket.handler.AbstractWebSocketHandler;
 public class WebSocketHandler extends AbstractWebSocketHandler {
 
   private final WebSocketSessionRegistry sessionRegistry;
+  private final NotificationService notificationService;
+  private final WebSocketSender webSocketSender;
+  private final ObjectMapper objectMapper;
 
   @Override
   public void afterConnectionEstablished(@NonNull WebSocketSession session) {
     String login = extractLogin(session);
     sessionRegistry.registerSession(login, session);
+
+    webSocketSender.sendNotificationsBySession(login, session);
   }
 
   @Override
@@ -33,9 +45,42 @@ public class WebSocketHandler extends AbstractWebSocketHandler {
   }
 
   @Override
-  protected void handleTextMessage(WebSocketSession session, @NonNull TextMessage message)
-      throws Exception {
-    session.sendMessage(message);
+  public void handleTextMessage(@NonNull WebSocketSession session, @NonNull TextMessage message) {
+    NotificationActionDto action = getAction(message.getPayload());
+    handleAction(action);
+
+    String login = extractLogin(session);
+    webSocketSender.sendNotificationsByLogin(login);
+  }
+
+  /**
+   * Handles the received notification action.
+   *
+   * @param actionDto the notification action to handle
+   * @throws ActionNotSupportedException if the action is not supported
+   */
+  private void handleAction(NotificationActionDto actionDto) {
+    long notificationId = actionDto.getNotificationId();
+    switch (NotificationAction.fromValue(actionDto.getAction())) {
+      case READ -> notificationService.readNotificationById(notificationId);
+      case DELETE -> notificationService.deleteNotificationById(notificationId);
+      case null -> throw new ActionNotSupportedException(actionDto.getAction());
+    }
+  }
+
+  /**
+   * Parses the incoming text message payload into a NotificationActionDto object.
+   *
+   * @param payload the payload of the incoming text message
+   * @return the parsed NotificationActionDto object
+   * @throws WebSocketInvalidMessageFormatException if the payload is not in the expected format
+   */
+  private NotificationActionDto getAction(String payload) {
+    try {
+      return objectMapper.readValue(payload, NotificationActionDto.class);
+    } catch (JsonProcessingException e) {
+      throw new WebSocketInvalidMessageFormatException(payload);
+    }
   }
 
   /**
