3e891c8e80532239a25561a65c050ac9007c938b
Ivan Ivashchenko <ivachko2009@gmail.com>
2024-04-02 13:52:43 +0300

    I-118 Rename UserInfoDto to UserDto

--- src/main/java/com/ratifire/devrate/mapper/impl/UserMapper.java
+++ src/main/java/com/ratifire/devrate/mapper/impl/UserMapper.java
@@ -1,6 +1,6 @@
 package com.ratifire.devrate.mapper.impl;
 
-import com.ratifire.devrate.dto.UserInfoDto;
+import com.ratifire.devrate.dto.UserDto;
 import com.ratifire.devrate.entity.User;
 import com.ratifire.devrate.mapper.DataMapper;
 import org.mapstruct.Mapper;
@@ -9,6 +9,6 @@ import org.mapstruct.Mapper;
  * Interface for mapping UserDto to User entities.
  */
 @Mapper(componentModel = "spring")
-public abstract class UserInfoMapper implements DataMapper<UserInfoDto, User> {
+public abstract class UserMapper implements DataMapper<UserDto, User> {
 
 }
