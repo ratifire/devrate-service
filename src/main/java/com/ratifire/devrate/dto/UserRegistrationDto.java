4c2edb178b3e92a1b9038e8c58490fc8a23d0156
hubersky <volodymyr.hubersky@gmail.com>
2024-04-03 15:46:45 +0300

    Resolve conflict

---
+++ src/main/java/com/ratifire/devrate/dto/UserRegistrationDto.java
@@ -0,0 +1,30 @@
+package com.ratifire.devrate.dto;
+
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * Data Transfer Object (DTO) representing the user registration.
+ */
+@Builder
+@Getter
+public class UserRegistrationDto {
+
+  private String email;
+
+  private String firstName;
+
+  private String lastName;
+
+  private String country;
+
+  private boolean subscribed;
+
+  private boolean verified;
+
+  private String password;
+
+  private LocalDateTime createdAt;
+
+}
--- src/main/java/com/ratifire/devrate/entity/User.java
+++ src/main/java/com/ratifire/devrate/entity/User.java
@@ -2,31 +2,18 @@ package com.ratifire.devrate.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 /**
- * Entity class representing a user in the system. A user is an entity with authentication
- * credentials and associated profile information.
+ * Entity class representing an entity for storing user personal information.
  */
 @Entity
 @Getter
@@ -35,86 +22,33 @@ import org.springframework.security.core.userdetails.UserDetails;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "users")
-public class User implements UserDetails {
+public class User {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
 
-  @Email
-  @Size(max = 100)
-  @Column(nullable = false, unique = true)
-  private String email;
-
-  @NotEmpty
-  @Size(max = 100)
   @Column(nullable = false)
   private String firstName;
 
-  @NotEmpty
-  @Size(max = 100)
   @Column(nullable = false)
   private String lastName;
 
-  @NotEmpty
-  @Size(max = 100)
+  private String position;
+
   @Column(nullable = false)
   private String country;
 
-  @NotNull
+  private String region;
+
+  @Column(nullable = false)
+  private String city;
+
   @Column(name = "is_subscribed", nullable = false)
   private boolean subscribed;
 
-  @NotNull
-  @Column(name = "is_verified", nullable = false)
-  private boolean verified;
+  private String description;
 
-  @NotNull
-  @Column(nullable = false)
-  private LocalDateTime createdAt;
-
-  @NotEmpty
-  @Size(max = 245)
-  @Column(nullable = false)
-  private String password;
-
-  @NotNull
-  @ManyToOne(fetch = FetchType.EAGER)
-  @JoinColumn(name = "role_id")
-  private Role role;
-
-  @Override
-  public Collection<? extends GrantedAuthority> getAuthorities() {
-    return List.of(role);
-  }
-
-  @Override
-  public String getPassword() {
-    return password;
-  }
-
-  @Override
-  public String getUsername() {
-    return email;
-  }
-
-  @Override
-  public boolean isAccountNonExpired() {
-    return true;
-  }
-
-  @Override
-  public boolean isAccountNonLocked() {
-    return true;
-  }
-
-  @Override
-  public boolean isCredentialsNonExpired() {
-    return true;
-  }
-
-  @Override
-  public boolean isEnabled() {
-    return verified;
-  }
+  @Column(name = "user_id", nullable = false)
+  private long userId;
 }
