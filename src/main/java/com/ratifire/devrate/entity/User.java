a448022406a8d1795590fec3e1aa71c36cf8ecc1
hubersky <volodymyr.hubersky@gmail.com>
2024-04-03 16:38:26 +0300

    Resolve: conflict

--- src/main/java/com/ratifire/devrate/entity/User.java
+++ src/main/java/com/ratifire/devrate/entity/User.java
@@ -2,14 +2,10 @@ package com.ratifire.devrate.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -56,7 +52,4 @@ public class User {
   @Column(name = "user_id", nullable = false)
   private long userId;
 
-  @OneToMany(fetch = FetchType.EAGER)
-  @JoinColumn(name = "user_id")
-  private List<Education> educations;
 }
