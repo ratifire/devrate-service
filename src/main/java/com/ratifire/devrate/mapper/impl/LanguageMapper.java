9652e5b1e3a87e9b1f678a9331eb6a02d16e5a04
Dmytro Abalmas <124811948+abalmasDA@users.noreply.github.com>
2024-04-04 19:13:06 +0300

    update language mapper class

--- src/main/java/com/ratifire/devrate/mapper/impl/LanguageMapper.java
+++ src/main/java/com/ratifire/devrate/mapper/impl/LanguageMapper.java
@@ -4,11 +4,20 @@ import com.ratifire.devrate.dto.LanguageDto;
 import com.ratifire.devrate.entity.Language;
 import com.ratifire.devrate.mapper.DataMapper;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 /**
  * Mapper abstract class for mapping between Language and LanguageDto objects.
  */
 @Mapper(componentModel = "spring")
 public abstract class LanguageMapper implements DataMapper<LanguageDto, Language> {
+
+  @Mapping(target = "id", ignore = true)
+  public abstract Language toEntity(LanguageDto languageDto);
+
+  @Mapping(target = "id", ignore = true)
+  public abstract Language updateEntity(LanguageDto languageDto,
+      @MappingTarget Language language);
 
 }
