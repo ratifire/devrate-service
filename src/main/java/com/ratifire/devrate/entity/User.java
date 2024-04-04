6ad8f3a384a597e3ecb57450fe56691824979944
O. Voskresenskyy <ovoskresenskyy@gmail.com>
2024-04-04 14:12:22 +0200

    Added lazy fetching gor security

--- src/main/java/com/ratifire/devrate/entity/User.java
+++ src/main/java/com/ratifire/devrate/entity/User.java
@@ -2,6 +2,7 @@ package com.ratifire.devrate.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -50,6 +51,6 @@ public class User {
 
   private String description;
 
-  @OneToOne(mappedBy = "user")
+  @OneToOne(mappedBy = "user", fetch = FetchType.LAZY)
   private UserSecurity security;
 }
