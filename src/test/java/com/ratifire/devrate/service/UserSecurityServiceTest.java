a4c46081813145c21322ee1c1f93f0c1c156b340
O. Voskresenskyy <ovoskresenskyy@gmail.com>
2024-04-11 19:23:32 +0200

    Minor code style fix

--- src/test/java/com/ratifire/devrate/service/UserSecurityServiceTest.java
+++ src/test/java/com/ratifire/devrate/service/UserSecurityServiceTest.java
@@ -21,7 +21,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 /**
  * Test class for the {@link UserSecurityService}.
