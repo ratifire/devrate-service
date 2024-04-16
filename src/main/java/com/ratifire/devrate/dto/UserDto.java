b8f6cbe4bf77eb02ce3184d5b720eb7546e3073e
Ivan Ivashchenko <ivachko2009@gmail.com>
2024-04-16 21:54:26 +0300

    I-170 Delete list of ContactDto and EducationDto from UserDto

--- src/main/java/com/ratifire/devrate/dto/UserDto.java
+++ src/main/java/com/ratifire/devrate/dto/UserDto.java
@@ -2,7 +2,6 @@ package com.ratifire.devrate.dto;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -37,8 +36,4 @@ public class UserDto {
   private boolean subscribed;
 
   private String description;
-
-  private List<ContactDto> contacts;
-
-  private List<EducationDto> educations;
 }
