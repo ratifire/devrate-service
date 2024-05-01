6e14bb8398093a80064eb268cd33c7fcb55c5631
hubersky <volodymyr.hubersky@gmail.com>
2024-05-01 12:44:34 +0300

    Add: Achievement Validation

--- src/main/java/com/ratifire/devrate/dto/AchievementDto.java
+++ src/main/java/com/ratifire/devrate/dto/AchievementDto.java
@@ -1,8 +1,11 @@
 package com.ratifire.devrate.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import org.hibernate.validator.constraints.URL;
 
 @Getter
 @Builder
@@ -11,10 +14,16 @@ public class AchievementDto {
 
   private long id;
 
+  @URL
+  @NotBlank(message = "must not be null or empty")
   private String link;
 
+  @Size(max = 65)
+  @NotBlank(message = "must not be null or empty")
   private String summary;
 
+  @Size(max = 150)
+  @NotBlank(message = "must not be null or empty")
   private String description;
 
 }
