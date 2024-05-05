b56d080b33bc27a719b58dee13e22dc5fc25b246
O. Voskresenskyy <ovoskresenskyy@gmail.com>
2024-05-05 17:42:19 +0200

    Deleted redundant authenticationManager Added throwing 401 exception instead of 403

--- src/main/java/com/ratifire/devrate/configuration/SecurityConfiguration.java
+++ src/main/java/com/ratifire/devrate/configuration/SecurityConfiguration.java
@@ -4,17 +4,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 /**
  * Configuration class for security settings.
@@ -42,12 +41,6 @@ public class SecurityConfiguration {
     return new BCryptPasswordEncoder();
   }
 
-  @Bean
-  public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
-      throws Exception {
-    return configuration.getAuthenticationManager();
-  }
-
   /**
    * Configures security filters.
    *
@@ -60,11 +53,12 @@ public class SecurityConfiguration {
     return http
         .csrf(AbstractHttpConfigurer::disable)
         .cors(AbstractHttpConfigurer::disable)
-        .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
         .authorizeHttpRequests((authorize) -> authorize
             .requestMatchers(WHITELIST).permitAll()
             .anyRequest().authenticated()
         )
+        .exceptionHandling(
+            e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
         .build();
   }
 
