5c9e251b2c421f7bb31629ae3559fc975be0e2e1
O. Voskresenskyy <ovoskresenskyy@gmail.com>
2024-05-10 12:25:07 +0200

    Fix import

--- src/main/java/com/ratifire/devrate/configuration/SecurityConfiguration.java
+++ src/main/java/com/ratifire/devrate/configuration/SecurityConfiguration.java
@@ -17,8 +17,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 /**
  * Configuration class for security settings.
