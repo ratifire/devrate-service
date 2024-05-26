3d796758f2ac5edc3c827ff88e95c1f2b19af4d4
Dmytro Abalmas <124811948+abalmasDA@users.noreply.github.com>
2024-05-26 16:34:16 +0300

    add Language Proficiency Data Mapper class

---
+++ src/main/java/com/ratifire/devrate/mapper/LanguageProficiencyDataMapper.java
@@ -0,0 +1,28 @@
+package com.ratifire.devrate.mapper;
+
+import com.ratifire.devrate.dto.LanguageProficiencyDataDto;
+import com.ratifire.devrate.enums.LanguageProficiencyLevel;
+import com.ratifire.devrate.enums.LanguageProficiencyName;
+import java.util.List;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+/**
+ * Mapper interface for converting LanguageProficiencyName enum values to LanguageProficiencyDataDto
+ * Data Transfer Objects (DTOs).
+ */
+@Mapper(componentModel = "spring")
+public interface LanguageProficiencyDataMapper {
+
+  @Mapping(source = "name", target = "name")
+  @Mapping(source = "code", target = "code")
+  @Mapping(target = "levels", expression = "java(mapLevelsByLanguage(languageProficiencyName))")
+  LanguageProficiencyDataDto toDto(LanguageProficiencyName languageProficiencyName);
+
+  List<LanguageProficiencyDataDto> toDto(List<LanguageProficiencyName> languageProficiencyNames);
+
+  default List<String> mapLevelsByLanguage(LanguageProficiencyName languageProficiencyName) {
+    return LanguageProficiencyLevel.getLevelsByLanguage(languageProficiencyName.getCode());
+  }
+
+}
