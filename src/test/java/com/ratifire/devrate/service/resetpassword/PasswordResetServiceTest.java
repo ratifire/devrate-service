7e94d9fe60602c63b7fa5a598b8fa17623416354
Serhii Martyniuk <weistapki@gmail.com>
2024-06-03 18:30:55 +0200

    fix

--- src/test/java/com/ratifire/devrate/service/resetpassword/PasswordResetServiceTest.java
+++ src/test/java/com/ratifire/devrate/service/resetpassword/PasswordResetServiceTest.java
@@ -1,6 +1,5 @@
 package com.ratifire.devrate.service.resetpassword;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doNothing;
