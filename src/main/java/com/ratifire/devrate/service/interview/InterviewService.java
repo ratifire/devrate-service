2885ee8d2e42675f971cbe750fb15e541c14fd6e
O. Voskresenskyy <ovoskresenskyy@gmail.com>
2024-06-23 13:25:35 +0200

    Deleted redundant controller. Used logic of creating meeting due to creating of the interview.

--- src/main/java/com/ratifire/devrate/service/interview/InterviewService.java
+++ src/main/java/com/ratifire/devrate/service/interview/InterviewService.java
@@ -3,10 +3,15 @@ package com.ratifire.devrate.service.interview;
 import com.ratifire.devrate.entity.interview.Interview;
 import com.ratifire.devrate.entity.interview.InterviewRequest;
 import com.ratifire.devrate.repository.interview.InterviewRepository;
+import com.ratifire.devrate.util.zoom.service.ZoomApiService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
 /**
  * Service for handling Interview operations.
@@ -15,24 +20,31 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class InterviewService {
 
-  private final InterviewRepository interviewRepository;
+    private final InterviewRepository interviewRepository;
+    private final ZoomApiService zoomApiService;
+    private static final Logger logger = LoggerFactory.getLogger(InterviewService.class);
 
-  /**
-   * Creates a new interview based on the provided candidate and interviewer requests.
-   *
-   * @param candidate   The request details for the candidate.
-   * @param interviewer The request details for the interviewer.
-   */
-  public void createInterview(InterviewRequest candidate, InterviewRequest interviewer) {
-    Interview interview = Interview.builder()
-        .candidateRequest(candidate)
-        .interviewerRequest(interviewer)
-        .startTime(
-            getMatchedStartTime(candidate.getAvailableDates(), interviewer.getAvailableDates()))
-        .build();
+    /**
+     * Creates a new interview based on the provided candidate and interviewer requests.
+     *
+     * @param candidate   The request details for the candidate.
+     * @param interviewer The request details for the interviewer.
+     */
+    public void createInterview(InterviewRequest candidate, InterviewRequest interviewer) {
+        zoomApiService.createMeeting("Topic", "Agenda", LocalDateTime.now())
+                .ifPresentOrElse(zoomMeeting -> {
+                    Interview interview = Interview.builder()
+                            .candidateRequest(candidate)
+                            .interviewerRequest(interviewer)
+                            .startTime(
+                                    getMatchedStartTime(candidate.getAvailableDates(), interviewer.getAvailableDates()))
+                            .zoomMeetingId(zoomMeeting.id)
+                            .build();
 
-    interviewRepository.save(interview);
-  }
+                    interviewRepository.save(interview);
+                }, () -> logger.debug("Interview for candidate {} and interviewer {} was not created",
+                        candidate, interviewer));
+    }
 
   /**
    * Finds the matched start time from the provided candidate and interviewer dates.
