7568224395019126ef720a2f27c4b6f67a0e9041
Dmytro Abalmas <124811948+abalmasDA@users.noreply.github.com>
2024-06-23 14:06:31 +0300

    update InterviewRequestCleanUpService

--- src/main/java/com/ratifire/devrate/service/interview/InterviewRequestCleanUpService.java
+++ src/main/java/com/ratifire/devrate/service/interview/InterviewRequestCleanUpService.java
@@ -2,9 +2,9 @@ package com.ratifire.devrate.service.interview;
 
 import com.ratifire.devrate.entity.User;
 import com.ratifire.devrate.entity.interview.InterviewRequest;
-import com.ratifire.devrate.repository.UserSecurityRepository;
 import com.ratifire.devrate.repository.interview.InterviewRequestRepository;
 import com.ratifire.devrate.service.NotificationService;
+import com.ratifire.devrate.service.UserSecurityService;
 import com.ratifire.devrate.service.email.EmailService;
 import java.time.ZonedDateTime;
 import java.util.List;
@@ -23,24 +23,24 @@ public class InterviewRequestCleanUpService {
   private static final long CLEANUP_INTERVAL = 21600000L; // 6 hours in milliseconds
 
   private final InterviewRequestRepository interviewRequestRepository;
-  private final UserSecurityRepository userSecurityRepository;
+  private final UserSecurityService userSecurityService;
   private final NotificationService notificationService;
   private final EmailService emailService;
 
   /**
    * Periodically cleans up expired interview requests. This method is scheduled to run at a fixed
-   * interval to find and delete interview requests that have expired.
+   * interval to find and delete interview requests that have expired and are still active.
    */
   @Scheduled(fixedRate = CLEANUP_INTERVAL, initialDelay = 86400000)
   @Transactional
-  public void deleteExpiredInterviewRequestsTask() {
+  public void deleteExpiredAndActiveInterviewRequestsTask() {
     ZonedDateTime currentDateTime = ZonedDateTime.now();
-    List<InterviewRequest> expiredRequests = interviewRequestRepository.findByExpiredAtBefore(
-        currentDateTime);
+    List<InterviewRequest> expiredRequests =
+        interviewRequestRepository.findByActiveTrueAndExpiredAtBefore(currentDateTime);
 
     for (InterviewRequest interviewRequest : expiredRequests) {
       User user = interviewRequest.getUser();
-      String email = userSecurityRepository.findEmailByUserId(user.getId());
+      String email = userSecurityService.findEmailByUserId(user.getId());
       sendInterviewRequestExpiryAlerts(user, email);
     }
 
