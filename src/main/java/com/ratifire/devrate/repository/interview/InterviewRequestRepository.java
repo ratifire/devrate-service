f7431366de55f2f54ff8fc280d511962a9326514
O. Voskresenskyy <ovoskresenskyy@gmail.com>
2024-06-30 14:14:40 +0200

    Fixed matching issue

--- src/main/java/com/ratifire/devrate/repository/interview/InterviewRequestRepository.java
+++ src/main/java/com/ratifire/devrate/repository/interview/InterviewRequestRepository.java
@@ -24,6 +24,7 @@ public interface InterviewRequestRepository extends JpaRepository<InterviewReque
           AND req.active = true
           AND req.id != :#{#request.id}
           AND req.role != :#{#request.role}
+          AND req.user != :#{#request.user}
           AND mast.level <= :#{#request.mastery.level}
           AND EXISTS (SELECT 1 FROM req.availableDates d WHERE d IN :#{#request.availableDates})
       """)
@@ -37,6 +38,7 @@ public interface InterviewRequestRepository extends JpaRepository<InterviewReque
           AND req.active = true
           AND req.id != :#{#request.id}
           AND req.role != :#{#request.role}
+          AND req.user != :#{#request.user}
           AND mast.level >= :#{#request.mastery.level}
           AND EXISTS (SELECT 1 FROM req.availableDates d WHERE d IN :#{#request.availableDates})
       """)
