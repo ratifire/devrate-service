45fd84482a2b2ccd8b2e5c384f9e65f241b9c197
Dmytro Abalmas <124811948+abalmasDA@users.noreply.github.com>
2024-08-02 14:59:14 +0300

    minor update

--- src/main/java/com/ratifire/devrate/validation/validators/ContactValueValidator.java
+++ src/main/java/com/ratifire/devrate/validation/validators/ContactValueValidator.java
@@ -18,7 +18,7 @@ public class ContactValueValidator implements
     ConstraintValidator<ValidateContactValue, ContactDto> {
 
   private static final Pattern EMAIL_PATTERN = Pattern.compile(
-      "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
+      "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$");
   private static final Pattern PHONE_PATTERN = Pattern.compile("^\\+?\\d{10,15}$");
   private static final String TELEGRAM_BASE_URL = "https://t.me/";
   private static final String LINKEDIN_BASE_URL = "https://www.linkedin.com/in/";
