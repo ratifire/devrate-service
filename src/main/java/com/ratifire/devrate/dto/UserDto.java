700e03eb0cb2367aa1275a87b3962300364e4297
Dmytro Abalmas <124811948+abalmasDA@users.noreply.github.com>
2024-08-04 23:09:09 +0300

    add validation to dto fields

--- src/main/java/com/ratifire/devrate/dto/UserDto.java
+++ src/main/java/com/ratifire/devrate/dto/UserDto.java
@@ -1,6 +1,8 @@
 package com.ratifire.devrate.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Builder;
@@ -20,19 +22,28 @@ public class UserDto {
   @NotNull
   private long id;
 
+  @NotBlank(message = "First name cannot be blank")
   @Size(max = 100)
+  @Pattern(regexp = "^[a-zA-Zа-щА-ЩґҐєЄіІїЇьЬ\\s\\-']+$", message = "First name can only contain "
+      + "letters, spaces, hyphens, and apostrophes")
   private String firstName;
 
+  @NotBlank(message = "First name cannot be blank")
   @Size(max = 100)
+  @Pattern(regexp = "^[a-zA-Zа-щА-ЩґҐєЄіІїЇьЬ\\s\\-']+$", message = "First name can only contain "
+      + "letters, spaces, hyphens, and apostrophes")
   private String lastName;
 
   @Size(max = 50)
   private String status;
 
+  @NotBlank(message = "Country name cannot be blank")
   @Size(max = 100)
   private String country;
 
   @Size(max = 100)
+  @Pattern(regexp = "^[a-zA-Zа-щА-ЩґҐєЄіІїЇьЬ\\s\\-']*$", message = "City name can only contain "
+      + "letters, spaces, hyphens, and apostrophes")
   private String city;
 
   private boolean subscribed;
--- src/main/java/com/ratifire/devrate/dto/UserRegistrationDto.java
+++ src/main/java/com/ratifire/devrate/dto/UserRegistrationDto.java
@@ -1,6 +1,9 @@
 package com.ratifire.devrate.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -11,17 +14,30 @@ import lombok.Getter;
 @Getter
 public class UserRegistrationDto {
 
+  @NotBlank(message = "Email cannot be blank")
+  @Size(max = 100)
   @Email
   private String email;
 
+  @NotBlank(message = "First name cannot be blank")
+  @Size(max = 100)
+  @Pattern(regexp = "^[a-zA-Zа-щА-ЩґҐєЄіІїЇьЬ\\s\\-']+$", message = "First name can only contain "
+      + "letters, spaces, hyphens, and apostrophes")
   private String firstName;
 
+  @NotBlank(message = "Last name cannot be blank")
+  @Size(max = 100)
+  @Pattern(regexp = "^[a-zA-Zа-щА-ЩґҐєЄіІїЇьЬ\\s\\-']+$", message = "First name can only contain "
+      + "letters, spaces, hyphens, and apostrophes")
   private String lastName;
 
+  @NotBlank(message = "Country cannot be blank")
+  @Size(max = 100)
   private String country;
 
   private boolean subscribed;
 
+  @NotBlank(message = "Password cannot be blank")
   private String password;
 
 }
