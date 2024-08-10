5f551caf6cd62a719f9303efa08f1c5be2004438
NewEnergyCode <matveychuk.m@gmail.com>
2024-08-10 16:07:43 +0300

    uncomment s3 bucket code.
    merge main into this branch.

--- terraform/deploy_service/main.tf
+++ terraform/deploy_service/main.tf
@@ -1,11 +1,11 @@
-# terraform {
-#   backend "s3" {
-#     bucket  = "devrate-bucket-back-1"
-#     encrypt = true
-#     key     = "AWS/backend-deploy-tstates/terraform.tfstate"
-#     region  = "eu-north-1"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket  = "devrate-bucket-back-1"
+    encrypt = true
+    key     = "AWS/backend-deploy-tstates/terraform.tfstate"
+    region  = "eu-north-1"
+  }
+}
 
 provider "aws" {}
 
--- terraform/infrastructure/main.tf
+++ terraform/infrastructure/main.tf
@@ -1,11 +1,11 @@
-# terraform {
-#   backend "s3" {
-#     bucket  = "devrate-bucket-back-1"
-#     encrypt = true
-#     key     = "AWS/back-infrastructure-tstates/terraform.tfstate"
-#     region  = "eu-north-1"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket  = "devrate-bucket-back-1"
+    encrypt = true
+    key     = "AWS/backend-infrastructure-tstates/terraform.tfstate"
+    region  = "eu-north-1"
+  }
+}
 
 provider "aws" {}
 
