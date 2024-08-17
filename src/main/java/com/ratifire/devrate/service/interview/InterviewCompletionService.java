637f38b53e946081e3a3f4dc450d84636ca24c73
Dmytro Abalmas <124811948+abalmasDA@users.noreply.github.com>
2024-08-17 12:35:36 +0300

    update InterviewCompletionService

--- src/main/java/com/ratifire/devrate/service/interview/InterviewCompletionService.java
+++ src/main/java/com/ratifire/devrate/service/interview/InterviewCompletionService.java
@@ -2,7 +2,9 @@ package com.ratifire.devrate.service.interview;
 
 import com.ratifire.devrate.entity.interview.Interview;
 import com.ratifire.devrate.service.specialization.SpecializationService;
+import com.ratifire.devrate.service.user.UserService;
 import com.ratifire.devrate.util.zoom.webhook.model.WebHookRequest.Payload.Meeting;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,7 +19,7 @@ public class InterviewCompletionService {
   private final InterviewService interviewService;
   private final InterviewSummaryService interviewSummaryService;
   private final SpecializationService specializationService;
-
+  private final UserService userService;
 
   /**
    * Completes the interview process by performing the necessary operations after a meeting has
@@ -30,7 +32,9 @@ public class InterviewCompletionService {
   public String completeInterviewProcess(Meeting meeting) {
     Interview interview = interviewService.getInterviewByMeetingId(Long.parseLong(meeting.getId()));
     interviewSummaryService.saveInterviewSummary(interview, meeting.getEndTime());
-    specializationService.updateInterviewCount(interview);
+    specializationService.updateUserInterviewCounts(interview);
+    userService.refreshUserInterviewCounts(List.of(interview.getInterviewerRequest().getUser(),
+        interview.getCandidateRequest().getUser()));
     //TODO: add the other needed logic for completing the interview process
     return "Interview process completed successfully!";
   }
