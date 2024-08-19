3f9286c213e71dcd0d927d68c5917e13c3f7ba42
NewEnergyCode <matveychuk.m@gmail.com>
2024-08-19 19:09:56 +0300

    refactoring pipeline for deploying to aws 11

--- terraform/deploy_service/task_defenition.tf
+++ terraform/deploy_service/task_defenition.tf
@@ -72,7 +72,7 @@ resource "aws_ecs_task_definition" "task_definition" {
     "EC2"
   ]
   cpu    = "1024"
-  memory = "980"
+  memory = "910"
   runtime_platform {
     operating_system_family = "LINUX"
     cpu_architecture        = "X86_64"
