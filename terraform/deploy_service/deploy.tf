56737e8511c9d6b48a32228a62a53384b23e978d
NewEnergyCode <matveychuk.m@gmail.com>
2024-08-20 10:42:53 +0300

    refactoring pipeline for deploying to aws 18

--- terraform/deploy_service/deploy.tf
+++ terraform/deploy_service/deploy.tf
@@ -110,7 +110,7 @@ resource "aws_ecs_service" "back_services" {
   scheduling_strategy                = "REPLICA"
   desired_count                      = 1
   force_new_deployment               = true
-  deployment_minimum_healthy_percent = 50
+  deployment_minimum_healthy_percent = 100
   deployment_maximum_percent         = 200
   capacity_provider_strategy {
     capacity_provider = aws_ecs_capacity_provider.back_capacity_provider.name
@@ -146,13 +146,6 @@ resource "aws_lb_target_group" "ecs_tg" {
   port     = 8080
   protocol = "HTTP"
   vpc_id   = data.aws_vpcs.all_vpcs.ids[0]
-  health_check {
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    interval            = 10
-    timeout             = 5
-    protocol            = "HTTP"
-  }
 }
 
 resource "aws_lb_listener" "ecs_listener" {
