c320643d88e88262266f92e89a6694e6b32d4a1c
NewEnergyCode <matveychuk.m@gmail.com>
2024-09-09 22:26:23 +0300

    set domain name config

--- terraform/deploy_service/set_domain.tf
+++ terraform/deploy_service/set_domain.tf
@@ -18,6 +18,21 @@ resource "aws_acm_certificate" "devrate_cert" {
 }
 
 
+resource "aws_lb_target_group" "https_ecs_tg" {
+  name     = "https-ecs-tg"
+  port     = 8080
+  protocol = "HTTPS"
+
+  vpc_id = data.aws_vpcs.all_vpcs.ids[0]
+  health_check {
+    healthy_threshold   = 4
+    unhealthy_threshold = 3
+    interval            = 60
+    protocol            = "HTTPS"
+    path                = "/actuator/health"
+  }
+}
+
 resource "aws_lb_listener" "https_ecs_listener" {
   load_balancer_arn = aws_lb.back_ecs_alb.arn
   port              = 443
@@ -26,7 +41,7 @@ resource "aws_lb_listener" "https_ecs_listener" {
   certificate_arn   = aws_acm_certificate.devrate_cert.arn
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.http_ecs_tg.arn
+    target_group_arn = aws_lb_target_group.https_ecs_tg.arn
   }
 }
 
