825c44e6fca6d96242174369677ab64e09316306
NewEnergyCode <matveychuk.m@gmail.com>
2024-09-09 21:08:23 +0300

    set domain name config

--- terraform/deploy_service/set_domain.tf
+++ terraform/deploy_service/set_domain.tf
@@ -1,7 +1,7 @@
 
 resource "aws_route53_record" "back_a_record" {
   zone_id = data.aws_route53_zone.dns_back_zone.zone_id
-  name    = "app.devrate.org"
+  name    = "devrate.org"
   type    = "A"
 
   alias {
@@ -20,7 +20,7 @@ resource "aws_acm_certificate" "devrate_cert" {
 
 resource "aws_lb_target_group" "https_ecs_tg" {
   name     = "https-ecs-tg"
-  port     = 443
+  port     = 8080
   protocol = "HTTPS"
 
   vpc_id = data.aws_vpcs.all_vpcs.ids[0]
@@ -35,7 +35,7 @@ resource "aws_lb_target_group" "https_ecs_tg" {
 
 resource "aws_lb_listener" "https_ecs_listener" {
   load_balancer_arn = aws_lb.back_ecs_alb.arn
-  port              = 443
+  port              = 8080
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
   certificate_arn   = aws_acm_certificate.devrate_cert.arn
