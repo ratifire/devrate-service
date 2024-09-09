8ce6543f9d3708127a67415489da66387e344b9d
NewEnergyCode <matveychuk.m@gmail.com>
2024-09-09 21:31:46 +0300

    set domain name config

--- terraform/deploy_service/set_domain.tf
+++ terraform/deploy_service/set_domain.tf
@@ -35,7 +35,7 @@ resource "aws_lb_target_group" "https_ecs_tg" {
 
 resource "aws_lb_listener" "https_ecs_listener" {
   load_balancer_arn = aws_lb.back_ecs_alb.arn
-  port              = 8080
+  port              = 443
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
   certificate_arn   = aws_acm_certificate.devrate_cert.arn
