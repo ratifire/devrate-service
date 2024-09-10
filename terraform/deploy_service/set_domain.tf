3c9df31fc32e269478175b490fd8a4fa265ee7ab
NewEnergyCode <matveychuk.m@gmail.com>
2024-09-10 10:21:48 +0300

    fmt

--- terraform/deploy_service/set_domain.tf
+++ terraform/deploy_service/set_domain.tf
@@ -13,9 +13,9 @@ resource "aws_route53_record" "back_a_record" {
 
 
 resource "aws_acm_certificate" "devrate_cert" {
-  domain_name       = "devrate.org"
+  domain_name               = "devrate.org"
   subject_alternative_names = ["back.devrate.org"]
-  validation_method = "DNS"
+  validation_method         = "DNS"
 }
 
 
