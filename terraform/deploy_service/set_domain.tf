732a7cf116b9612895feef5508c4455bcce633c3
NewEnergyCode <matveychuk.m@gmail.com>
2024-09-11 22:16:09 +0300

    renaming

--- terraform/deploy_service/set_domain.tf
+++ terraform/deploy_service/set_domain.tf
@@ -1,6 +1,10 @@
 resource "aws_acm_certificate" "devrate_cert" {
   domain_name       = var.domain_name
   validation_method = "DNS"
+
+  subject_alternative_names = [
+    "*.devrate.org"
+  ]
 }
 
 
