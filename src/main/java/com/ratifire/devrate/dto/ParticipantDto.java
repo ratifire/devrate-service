ff0d77859ae0d44b91989b1d1ab5d47bfdc9a784
Ivan Ivashchenko <62796151+IvashchenkoIvanIgorevich@users.noreply.github.com>
2024-09-20 12:42:26 +0300

    I-494 Added user ID to the ParticipantDto (#498)
    * I-494 Added user ID to the ParticipantDto
    
    * I-494 Swagger changes

--- src/main/java/com/ratifire/devrate/dto/ParticipantDto.java
+++ src/main/java/com/ratifire/devrate/dto/ParticipantDto.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ParticipantDto {
+  private long id;
   private String name;
   private String surname;
   private String status;
