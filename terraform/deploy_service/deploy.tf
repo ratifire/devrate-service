522d0dac3b7f324358ec19bfe75f708ebcd00b22
NewEnergyCode <matveychuk.m@gmail.com>
2024-09-23 19:57:59 +0300

    added 2 instance into service

--- terraform/deploy_service/deploy.tf
+++ terraform/deploy_service/deploy.tf
@@ -148,7 +148,7 @@ resource "aws_lb_target_group" "http_ecs_back_tg" {
   vpc_id               = data.aws_vpcs.all_vpcs.ids[0]
   deregistration_delay = "120"
   health_check {
-    healthy_threshold   = 1
+    healthy_threshold   = 2
     unhealthy_threshold = 2
     interval            = 60
     protocol            = "HTTP"
