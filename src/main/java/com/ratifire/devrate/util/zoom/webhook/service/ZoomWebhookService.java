460cb439f854b1e763e13fed111fc120fa2c3b6b
Serhii Martyniuk <weistapki@gmail.com>
2024-09-24 18:22:01 +0200

    Added logic to ignore meetings that ended before the scheduled time + 10 minutes.

--- src/main/java/com/ratifire/devrate/util/zoom/webhook/service/ZoomWebhookService.java
+++ src/main/java/com/ratifire/devrate/util/zoom/webhook/service/ZoomWebhookService.java
@@ -2,9 +2,12 @@ package com.ratifire.devrate.util.zoom.webhook.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ratifire.devrate.entity.interview.Interview;
 import com.ratifire.devrate.service.interview.InterviewCompletionService;
+import com.ratifire.devrate.service.interview.InterviewService;
 import com.ratifire.devrate.util.zoom.webhook.exception.ZoomWebhookException;
 import com.ratifire.devrate.util.zoom.webhook.model.WebHookRequest;
+import java.time.ZonedDateTime;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,6 +23,7 @@ public class ZoomWebhookService {
   private final ZoomWebhookAuthService zoomWebhookAuthService;
   private final InterviewCompletionService interviewCompletionService;
   private final ObjectMapper objectMapper;
+  private final InterviewService interviewService;
 
   /**
    * Handles all webhook events from Zoom.
@@ -36,16 +40,33 @@ public class ZoomWebhookService {
 
     String signature = headers.get("x-zm-signature");
     String timestamp = headers.get("x-zm-request-timestamp");
-
     zoomWebhookAuthService.validateSignature(signature, requestBody, timestamp);
 
     WebHookRequest payload = objectMapper.readValue(requestBody, WebHookRequest.class);
     String event = payload.getEvent();
-
     return switch (event) {
       case "endpoint.url_validation" -> zoomWebhookAuthService.handleUrlValidationEvent(payload);
-      case "meeting.ended" ->
-          interviewCompletionService.completeInterviewProcess(payload.getPayload().getMeeting());
+      case "meeting.ended" -> {
+        long meetingId = Long.parseLong(payload.getPayload().getMeeting().getId());
+
+        Interview interview = interviewService.getInterviewByMeetingId(meetingId);
+        ZonedDateTime scheduledStartTime = interview.getStartTime();
+        String endTime = payload.getPayload().getMeeting().getEndTime();
+
+        if (endTime == null) {
+          throw new ZoomWebhookException("End time is missing in the webhook payload.");
+        }
+        ZonedDateTime webhookTime = ZonedDateTime.parse(payload.getPayload()
+            .getMeeting().getEndTime());
+
+        if (webhookTime.isBefore(scheduledStartTime.plusMinutes(10))) {
+          yield "Too early to process the meeting. Ignoring webhook.";
+        }
+
+        yield interviewCompletionService.completeInterviewProcess(payload
+            .getPayload().getMeeting());
+
+      }
       default -> throw new ZoomWebhookException("Unknown event type");
     };
   }
