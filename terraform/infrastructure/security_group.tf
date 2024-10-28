29cf58ab4ca802dfe4253657e732948f92bc6eb5
NewEnergyCode <matveychuk.m@gmail.com>
2024-10-28 11:27:00 +0200

    added slack notification

--- terraform/infrastructure/security_group.tf
+++ terraform/infrastructure/security_group.tf
@@ -47,7 +47,7 @@ resource "aws_default_subnet" "default_az3" {
 
 resource "aws_subnet" "private_subnet_db" {
   vpc_id                  = aws_default_vpc.default_vpc.id
-  cidr_block              = "172.31.24.0/24"
+  cidr_block              = "172.31.24.0/16"
   availability_zone       = "eu-north-1a"
   map_public_ip_on_launch = false
 }
