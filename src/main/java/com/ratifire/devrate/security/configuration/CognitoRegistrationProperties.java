8c76893a2bf904a978ae7a5d7ca37a841ad50e6d
Dmytro Abalmas <124811948+abalmasDA@users.noreply.github.com>
2024-11-01 21:58:33 +0200

    Feat:Cognito: add properties classes

---
+++ src/main/java/com/ratifire/devrate/security/configuration/CognitoRegistrationProperties.java
@@ -0,0 +1,25 @@
+package com.ratifire.devrate.security.configuration;
+
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldNameConstants;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration properties for AWS Cognito registration within the OAuth2 client setup.
+ */
+@Configuration
+@ConfigurationProperties("spring.security.oauth2.client.registration.cognito")
+@FieldNameConstants
+@Getter
+@Setter
+public class CognitoRegistrationProperties {
+
+  private String clientName;
+  private String clientId;
+  private String clientSecret;
+  private List<String> scope;
+
+}
