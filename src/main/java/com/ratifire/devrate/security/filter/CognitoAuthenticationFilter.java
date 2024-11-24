f731b820ad982c168c1f1ab41451d4a755b29860
Dmytro Abalmas <124811948+abalmasDA@users.noreply.github.com>
2024-11-24 10:25:10 +0200

    Upd:Auth: update CognitoAuthenticationFilter

--- src/main/java/com/ratifire/devrate/security/filter/CognitoAuthenticationFilter.java
+++ src/main/java/com/ratifire/devrate/security/filter/CognitoAuthenticationFilter.java
@@ -1,8 +1,11 @@
 package com.ratifire.devrate.security.filter;
 
+import static com.ratifire.devrate.security.model.enums.AuthenticationError.*;
 import static com.ratifire.devrate.security.model.enums.CognitoTypeToken.ACCESS_TOKEN;
 import static com.ratifire.devrate.security.model.enums.CognitoTypeToken.ID_TOKEN;
 
+import com.ratifire.devrate.security.exception.AuthenticationException;
+import com.ratifire.devrate.security.exception.TokenExpiredException;
 import com.ratifire.devrate.security.service.CognitoTokenValidationService;
 import com.ratifire.devrate.security.util.TokenUtil;
 import jakarta.servlet.FilterChain;
@@ -25,6 +28,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 @RequiredArgsConstructor
 public class CognitoAuthenticationFilter extends OncePerRequestFilter {
 
+  private static final String AUTHENTICATION_ERROR_ATTRIBUTE = "authentication_error";
   private final CognitoTokenValidationService cognitoTokenValidationService;
 
   @Override
@@ -39,12 +43,23 @@ public class CognitoAuthenticationFilter extends OncePerRequestFilter {
       return;
     }
 
-    if (cognitoTokenValidationService.validateToken(accessToken, ACCESS_TOKEN)
-        && cognitoTokenValidationService.validateToken(idToken, ID_TOKEN)) {
-      setUpAuthenticationContext(idToken);
-    }
+    try {
 
-    filterChain.doFilter(request, response);
+      if (cognitoTokenValidationService.validateToken(accessToken, ACCESS_TOKEN)
+          && cognitoTokenValidationService.validateToken(idToken, ID_TOKEN)) {
+        setUpAuthenticationContext(idToken);
+      }
+
+      filterChain.doFilter(request, response);
+
+    } catch (TokenExpiredException e) {
+      request.setAttribute(AUTHENTICATION_ERROR_ATTRIBUTE, TOKEN_EXPIRED);
+      throw new TokenExpiredException("Authentication process was failed. Token has been expired");
+
+    } catch (Exception e) {
+      request.setAttribute(AUTHENTICATION_ERROR_ATTRIBUTE, UNAUTHORIZED);
+      throw new AuthenticationException("Authentication process was failed");
+    }
   }
 
   private void setUpAuthenticationContext(String idToken) {
