228cb9754499275c69ccc726a5ef18e6e13311ba
Dmytro Abalmas <124811948+abalmasDA@users.noreply.github.com>
2024-11-29 22:15:57 +0200

    Feat:SSO: app CognitoOAuthHelper

---
+++ src/main/java/com/ratifire/devrate/security/helper/CognitoOAuthHelper.java
@@ -0,0 +1,11 @@
+package com.ratifire.devrate.security.helper;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CognitoOAuthHelper {
+  //TODO need to implement
+
+}
