4cee483cc94e85112a4c67e78f458ef286028aef
Dmytro Abalmas <124811948+abalmasDA@users.noreply.github.com>
2025-01-11 15:42:31 +0200

    add methods

--- src/main/java/com/ratifire/devrate/repository/interview/InterviewRequestRepository.java
+++ src/main/java/com/ratifire/devrate/repository/interview/InterviewRequestRepository.java
@@ -1,8 +1,11 @@
 package com.ratifire.devrate.repository.interview;
 
 import com.ratifire.devrate.entity.interview.InterviewRequest;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
@@ -15,4 +18,10 @@ public interface InterviewRequestRepository extends JpaRepository<InterviewReque
   Optional<InterviewRequest> findByIdAndUser_Id(long id, long userId);
 
   void deleteByIdAndUser_Id(long id, long userId);
+
+  @Query("SELECT r.mastery.id FROM InterviewRequest r WHERE r.id = :id")
+  Optional<Long> findMasteryIdById(@Param("id") Long id);
+
+  @Query("SELECT r FROM InterviewRequest r WHERE r.id IN (:ids)")
+  List<InterviewRequest> findByIds(@Param("ids") List<Long> ids);
 }
