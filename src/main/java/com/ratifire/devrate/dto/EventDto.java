42dd8e0cf338953df869ade1519769c185175d29
Dmytro Abalmas <124811948+abalmasDA@users.noreply.github.com>
2025-01-18 13:17:26 +0200

    update event dto

--- src/main/java/com/ratifire/devrate/dto/EventDto.java
+++ src/main/java/com/ratifire/devrate/dto/EventDto.java
@@ -2,12 +2,10 @@ package com.ratifire.devrate.dto;
 
 import com.ratifire.devrate.enums.EventType;
 import java.time.ZonedDateTime;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.URL;
 
 /**
  * Data Transfer Object representing the calendar event.
@@ -19,17 +17,8 @@ import org.hibernate.validator.constraints.URL;
 public class EventDto {
 
   private long id;
-  /**
-   * Identifier related to the specific event type.
-   * For example, if the event type is 'interview', this field will store 'interviewId'.
-   * If the event type is 'conference', this field will store 'conferenceId', etc.
-   */
-  private long eventTypeId;
   private EventType type;
-  @URL
-  private String link;
-  private ParticipantDto host;
-  private List<ParticipantDto> participantDtos;
+  private String roomLink;
   private ZonedDateTime startTime;
 
 }
