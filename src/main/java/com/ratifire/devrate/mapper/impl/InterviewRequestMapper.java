2aca0ed67cbf4cc8e9d604e4011942361914d75f
Ivan Ivashchenko <62796151+IvashchenkoIvanIgorevich@users.noreply.github.com>
2025-01-19 14:31:53 +0200

    I-663 Add endpoints for InterviewRequest (#665)
    * I-663 Added UI endpoint
    
    * I-663 Refactoring swagger
    
    * I-663 Populate assigned date to the scheduled interview request

--- src/main/java/com/ratifire/devrate/mapper/impl/InterviewRequestMapper.java
+++ src/main/java/com/ratifire/devrate/mapper/impl/InterviewRequestMapper.java
@@ -4,13 +4,9 @@ import com.ratifire.devrate.dto.InterviewRequestDto;
 import com.ratifire.devrate.entity.interview.InterviewRequest;
 import com.ratifire.devrate.mapper.DataMapper;
 import com.ratifire.devrate.service.MasteryService;
-import java.time.ZonedDateTime;
-import java.util.Collections;
-import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
-import org.mapstruct.Named;
 
 /**
  * Mapper for converting between InterviewRequestDto and InterviewRequestV2 entities.
@@ -22,31 +18,13 @@ public abstract class InterviewRequestMapper implements
   @Mapping(target = "id", ignore = true)
   @Mapping(target = "mastery", source = "dto.masteryId",
       qualifiedByName = {"MasteryService", "getMasteryById"})
-  @Mapping(target = "expiredAt", source = "dto.availableDates",
-      qualifiedByName = "expiredAtFromAvailableDates")
   @Mapping(target = "user", ignore = true)
   public abstract InterviewRequest toEntity(InterviewRequestDto dto);
 
   @Mapping(target = "id", ignore = true)
   @Mapping(target = "mastery", source = "dto.masteryId",
       qualifiedByName = {"MasteryService", "getMasteryById"})
-  @Mapping(target = "expiredAt", source = "dto.availableDates",
-      qualifiedByName = "expiredAtFromAvailableDates")
   @Mapping(target = "user", ignore = true)
   public abstract InterviewRequest updateEntity(InterviewRequestDto dto,
       @MappingTarget InterviewRequest entity);
-
-  @Mapping(target = "masteryId", source = "entity.mastery.id")
-  public abstract InterviewRequestDto toDto(InterviewRequest entity);
-
-  /**
-   * Returns the latest date from the given list of dates.
-   *
-   * @param availableDates a list of ZonedDateTime objects
-   * @return the latest ZonedDateTime from the list
-   */
-  @Named("expiredAtFromAvailableDates")
-  public static ZonedDateTime expiredAtFromAvailableDates(List<ZonedDateTime> availableDates) {
-    return Collections.max(availableDates);
-  }
 }
