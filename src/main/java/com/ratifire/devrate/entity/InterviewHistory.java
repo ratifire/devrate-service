b8e499db699f337d8c799cf784676c2feb23e882
Ivan Ivashchenko <ivachko2009@gmail.com>
2025-02-04 12:57:51 +0200

    Added new fields InterviewHistoryDto and mock data

--- src/main/java/com/ratifire/devrate/entity/InterviewHistory.java
+++ src/main/java/com/ratifire/devrate/entity/InterviewHistory.java
@@ -13,7 +13,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.MapKeyColumn;
 import jakarta.persistence.Table;
-import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 import java.util.Map;
 import lombok.AllArgsConstructor;
@@ -57,14 +56,23 @@ public class InterviewHistory {
   @Column(name = "skill_value")
   private Map<String, Integer> hardSkills;
 
-  @Column(name = "title", nullable = false, length = 255)
-  private String title;
+  @Column(name = "specialization", nullable = false, length = 255)
+  private String specialization;
+
+  @Column(name = "mastery_level", nullable = false)
+  private int masteryLevel;
 
   @Enumerated(EnumType.STRING)
   private InterviewRequestRole role;
 
   @Column(name = "attendee_id", nullable = false)
   private int attendeeId;
+
+  @Column(name = "attendee_mastery_level", nullable = false)
+  private int attendeeMasteryLevel;
+
+  @Column(name = "attendee_specialization", nullable = false)
+  private String attendeeSpecialization;
 
   @Column(length = 1000)
   private String feedback;
