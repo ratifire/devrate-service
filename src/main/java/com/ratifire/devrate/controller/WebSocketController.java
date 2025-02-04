00ef669c995eb44df7a970dd236a1dc0ff6a056c
Ivan Ivashchenko <ivachko2009@gmail.com>
2025-02-04 22:17:48 +0200

    Minor websocket chat messaging

--- src/main/java/com/ratifire/devrate/controller/WebSocketController.java
+++ src/main/java/com/ratifire/devrate/controller/WebSocketController.java
@@ -25,6 +25,8 @@ public class WebSocketController {
   public void sendMessage(@RequestBody ChatMessageDto chatMessageDto) {
     simpMessagingTemplate.convertAndSend(
         String.format("/topic/messages/%s", chatMessageDto.getReceiverId()), chatMessageDto);
+    simpMessagingTemplate.convertAndSend(
+        String.format("/topic/messages/%s", chatMessageDto.getSenderId()), chatMessageDto);
 
     chatService.createMessage(chatMessageDto);
   }
