220894d691bde5e57770b2c4e3b39068b4b0f05a
Ivan Ivashchenko <ivachko2009@gmail.com>
2025-03-15 11:55:28 +0200

    Sorted interview history by dateTime field at the descending order direction

--- src/main/java/com/ratifire/devrate/service/interview/InterviewHistoryService.java
+++ src/main/java/com/ratifire/devrate/service/interview/InterviewHistoryService.java
@@ -30,6 +30,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -81,7 +82,7 @@ public class InterviewHistoryService {
    */
   public Page<InterviewHistoryDto> getAllByUserId(int page, int size) {
     long userId = userContextProvider.getAuthenticatedUserId();
-    Pageable pageable = PageRequest.of(page, size);
+    Pageable pageable = PageRequest.of(page, size, Sort.by("dateTime").descending());
     return interviewHistoryRepository.findAllByUserIdAndIsVisibleTrue(userId, pageable)
         .map(interviewHistoryMapper::toDto);
   }
