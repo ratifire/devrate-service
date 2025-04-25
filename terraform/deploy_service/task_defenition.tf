e0aabddbe2a4d0a5bfbd0d3fd33141367b10309f
MaksymMatveichuk <matveychuk.m@gmail.com>
2025-04-25 17:05:55 +0300

    add new relic to TD and workflow

--- terraform/deploy_service/task_defenition.tf
+++ terraform/deploy_service/task_defenition.tf
@@ -57,10 +57,6 @@ resource "aws_ecs_task_definition" "task_definition" {
         {
           name  = "NEW_RELIC_LOG_LEVEL"
           value = "info"
-        },
-        {
-          name  = "JAVA_OPTS"
-          value = "-javaagent:/path/to/newrelic.jar"
         }
       ],
       mountPoints = [],
@@ -75,12 +71,7 @@ resource "aws_ecs_task_definition" "task_definition" {
         },
         secretOptions = []
       },
-      systemControls = [],
-      command = [
-        "sh",
-        "-c",
-        "if [ -f /newrelic/newrelic.jar ]; then echo 'New Relic Agent found: $(java -jar /newrelic/newrelic.jar -version)'; else echo 'New Relic Agent NOT found!'; exit 1; fi && java -javaagent:/newrelic/newrelic.jar -jar /devRate.jar"
-      ]
+      systemControls = []
     }
   ])
 
