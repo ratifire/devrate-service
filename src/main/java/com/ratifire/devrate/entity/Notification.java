faf832d97ae38961add6ff30c2dbdaadf188f4e3
Dmytro Abalmas <124811948+abalmasDA@users.noreply.github.com>
2025-05-04 22:32:02 +0300

    fix notifications

--- src/main/java/com/ratifire/devrate/entity/Notification.java
+++ src/main/java/com/ratifire/devrate/entity/Notification.java
@@ -3,8 +3,6 @@ package com.ratifire.devrate.entity;
 import com.ratifire.devrate.enums.NotificationType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
