0a7b96eec41617359855b1049f2beef593ea3a78
Dmytro Abalmas <124811948+abalmasDA@users.noreply.github.com>
2025-05-04 16:20:32 +0300

    add liquibase implementation

--- src/main/java/com/ratifire/devrate/entity/Notification.java
+++ src/main/java/com/ratifire/devrate/entity/Notification.java
@@ -3,6 +3,8 @@ package com.ratifire.devrate.entity;
 import com.ratifire.devrate.enums.NotificationType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -37,6 +39,7 @@ public class Notification {
   @Column(columnDefinition = "JSONB")
   private String payload;
 
+  @Enumerated(EnumType.STRING)
   @Column(nullable = false)
   private NotificationType type;
 
