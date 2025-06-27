009bca7028510b0eab0e5f4ca6625fe2d2a688f1
Ivan Ivashchenko <ivachko2009@gmail.com>
2025-06-27 14:28:07 +0300

    I-766 PR changes

--- src/main/java/com/ratifire/devrate/controller/WebPushSubscriptionController.java
+++ src/main/java/com/ratifire/devrate/controller/WebPushSubscriptionController.java
@@ -1,15 +1,11 @@
 package com.ratifire.devrate.controller;
 
-import com.ratifire.devrate.dto.NotificationDto;
 import com.ratifire.devrate.dto.WebPushSubscriptionDto;
-import com.ratifire.devrate.service.WebPushNotificationService;
 import com.ratifire.devrate.service.WebPushSubscriptionService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -21,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class WebPushSubscriptionController {
 
   private final WebPushSubscriptionService subscriptionService;
-  private final WebPushNotificationService notificationService;
 
   @PostMapping("/subscribe")
   public void subscribe(@RequestBody WebPushSubscriptionDto dto) {
@@ -31,23 +26,5 @@ public class WebPushSubscriptionController {
   @PostMapping("/unsubscribe")
   public void unsubscribe(@RequestBody WebPushSubscriptionDto dto) {
     subscriptionService.deleteSubscription(dto.getEndpoint());
-  }
-
-  /**
-   * Send test web push notification message.
-   *
-   * @param userId  logged user id
-   * @param message message that will be sent
-   * @return status code
-   */
-  @PostMapping("/test-message/send")
-  public ResponseEntity<Void> sendTestNotification(@RequestParam Long userId,
-      @RequestBody NotificationDto message) {
-    try {
-      notificationService.sendNotification(userId, message);
-      return ResponseEntity.ok().build();
-    } catch (Exception e) {
-      return ResponseEntity.internalServerError().build();
-    }
   }
 }
