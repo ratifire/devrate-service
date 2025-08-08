e9235f3863cdee7cd7a16e682e72c39eed078cdb
MaksymMatveichuk <matveychuk.m@gmail.com>
2025-08-08 14:50:02 +0300

    upgrade security using private subnets

--- terraform/infrastructure/security_group.tf
+++ terraform/infrastructure/security_group.tf
@@ -26,7 +26,7 @@ resource "aws_subnet" "public_subnet" {
   count                   = 3
   vpc_id                  = local.vpc_id
   availability_zone       = local.azs[count.index]
-  cidr_block              = cidrsubnet("10.0.0.0/16", 8, count.index)
+  cidr_block              = cidrsubnet("10.1.0.0/16", 8, count.index)
   map_public_ip_on_launch = true
 
   tags = {
@@ -39,7 +39,7 @@ resource "aws_subnet" "private_subnet" {
   count                   = 3
   vpc_id                  = local.vpc_id
   availability_zone       = local.azs[count.index]
-  cidr_block              = cidrsubnet("10.0.0.0/16", 8, count.index + 10)
+  cidr_block              = cidrsubnet("10.1.0.0/16", 8, count.index + 10)
   map_public_ip_on_launch = false
   tags = {
     Name = "private-subnet-${var.deploy_profile}-${count.index + 1}"
