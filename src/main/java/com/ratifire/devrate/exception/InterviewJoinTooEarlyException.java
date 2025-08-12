248739afcdfbac9670b1dbee2bd3685fef459ed3
Ivan Ivashchenko <ivachko2009@gmail.com>
2025-08-12 16:43:30 +0300

    I-815 Fix swagger

--- src/main/java/com/ratifire/devrate/exception/InterviewJoinTooEarlyException.java
+++ src/main/java/com/ratifire/devrate/exception/InterviewJoinTooEarlyException.java
@@ -4,7 +4,11 @@ package com.ratifire.devrate.exception;
  * Exception thrown when a user attempts to join an interview meeting earlier than the allowed.
  */
 public class InterviewJoinTooEarlyException extends RuntimeException {
+
+  private static final String MESSAGE = "Joining is only permitted 5 minutes prior to the interview"
+      + " starting.";
+
   public InterviewJoinTooEarlyException() {
-    super("Joining is only permitted 5 minutes prior to the interview starting.");
+    super(MESSAGE);
   }
 }
